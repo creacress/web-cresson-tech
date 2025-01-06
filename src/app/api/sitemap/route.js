ec81d583c2ea071945cac26900e01f6b102ca83a
CRESSON Alexis <alexis@webcresson.com>
2025-01-06 10:06:36 +0100

    Change xmlns site maps

--- src/app/api/sitemap/route.js
+++ src/app/api/sitemap/route.js
@@ -2,34 +2,34 @@ export async function GET() {
   const baseUrl = "https://webcresson.com";
 
   const staticPaths = [
-    "/",
-    "/contact",
-    "/about",
-    "/services",
-    "/services/ia-generative",
-    "/services/maintenance-predictive",
-    "/services/rpa-ia",
-    "/services/ia-traitement-images",
-    "/services/ia-sur-mesure",
-    "/services/ia-archeologie",
-    "/services/pricing-analyse-avancee",
-    "/TermsOfSale",
-    "/LegalMentions"
+    { path: "/", changefreq: "always", priority: "1.0" },
+    { path: "/contact", changefreq: "daily", priority: "0.8" },
+    { path: "/about", changefreq: "daily", priority: "0.8" },
+    { path: "/services", changefreq: "daily", priority: "0.8" },
+    { path: "/services/ia-generative", changefreq: "daily", priority: "0.8" },
+    { path: "/services/maintenance-predictive", changefreq: "daily", priority: "0.8" },
+    { path: "/services/rpa-ia", changefreq: "daily", priority: "0.8" },
+    { path: "/services/ia-traitement-images", changefreq: "daily", priority: "0.8" },
+    { path: "/services/ia-sur-mesure", changefreq: "daily", priority: "0.8" },
+    { path: "/services/ia-archeologie", changefreq: "daily", priority: "0.8" },
+    { path: "/services/pricing-analyse-avancee", changefreq: "daily", priority: "0.8" },
+    { path: "/TermsOfSale", changefreq: "daily", priority: "0.8" },
+    { path: "/LegalMentions", changefreq: "daily", priority: "0.8" },
   ];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${staticPaths
     .map(
-      (path) => `
+      ({ path, changefreq, priority }) => `
   <url>
     <loc>${baseUrl}${path}</loc>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
+    <changefreq>${changefreq}</changefreq>
+    <priority>${priority}</priority>
   </url>`
     )
     .join("")}
-</urlset>`.trim(); // Ajout de trim() pour éviter les espaces superflus.
+</urlset>`.trim();
 
   return new Response(sitemap, {
     headers: {
